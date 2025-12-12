eaeaf08a86c6bae2aa1d35ff5c62c2d24af1c901
ToqaHussien <toqauae.2017@gmail.com>
2025-12-12 20:03:40 +0200

    prepare for build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  nitro: {
+    prerender: {
+      routes: ["/"],
+    },
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.scss"],
