753f73edf516f82b3e6edfd4892279bb621197ab
ToqaHussien <toqauae.2017@gmail.com>
2025-12-13 14:44:52 +0200

    favicon

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,15 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+useHead({
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/portfolio/favicon.png",
+    },
+  ],
+});
+</script>
